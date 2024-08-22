bda610239b23c927dde3bf22fd8241608e2518c8
Ajmal Hakim <samimajmal@yahoo.com>
2024-08-21 20:58:05 -0400

    add example for setTimout  * setIntervale func

--- js/app.js
+++ js/app.js
@@ -20,11 +20,72 @@ function greet(name){
 //greet("John Doe")
 
 
-const hof  = (callback)=>{
- const userName = prompt("Please enter your name?")
- callback(userName)
+// const hof  = (callback)=>{
+//  const userName = prompt("Please enter your name?")
+//  callback(userName)
+// }
+
+// hof(greet)
+// 12 (6) (6)  
+// req(titanic) => api => database  10
+
+
+// synchrounous 
+// Asynchronous 
+
+// 🚗
+// 🚓
+// beats 
+
+// calll 50 
+// Hello
+// mininute 
+// 6o 
+// 1000ms
+
+let first = ()=> console.log("Hello, First");
+let  second = ()=> {
+  setTimeout(function(){
+    console.log("Hello, Second");
+  },5000)
 }
 
-hof(greet)
+first();
+second();
+console.log("Hello , I am here");
+
+
+
+const learnGit = ()=> console.log('Hello, Everyone');
+
+setTimeout(learnGit,3000)
+
+
+function asyncOperation(func) {
+  console.log("Operation Started ...");
+
+  setTimeout(()=> {
+    console.log('Hello, Everyone');
+    func();
+  },2000)
+  
+}
+
+
+asyncOperation(()=>{
+  console.log("Callback execured after operation completion");
+  
+})
+
+
+
+function displayTime(){
+
+  console.log("Current Time " + new Date().toLocaleTimeString());
+  
+}
+
+//setInterval(displayTime,5000)
+
 
 
